4649b0f74e4887369416d61f209bb53351d404bb
nbproduction433 <85150514+nbproduction433@users.noreply.github.com>
2024-03-14 21:13:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ client.on('ready', async () => {
     .setURL('https://www.youtube.com/') //
     .setState('an open-world')//تحط اي اسم تبيه يظهر بالحاله
     .setName('I fail in your love')//تحط اي اسم تبيه يظهر بالحاله
-    .setDetails(`Genshin Impact)//تحط اي اسم تبيه يظهر بالحاله
+    .setDetails(`Genshin Impact')//تحط اي اسم تبيه يظهر بالحاله
     .setStartTimestamp(Date.now())
  .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1107569693373509715/1217851430300553227/ffbab8235ddc5c0290ecd6ceccc0a61a.png?ex=66058787&is=65f31287&hm=37b982b132ecde35987cac720f189c81d780fa1424c0e13acccd1c464f45badf&') //رابط الصوره الكبيره.
 
