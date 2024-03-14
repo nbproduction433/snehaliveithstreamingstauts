a6ca47d519d52b85cc6288644cebaea919196cb3
nbproduction433 <85150514+nbproduction433@users.noreply.github.com>
2024-03-14 21:08:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,14 +26,14 @@ client.on('ready', async () => {
     .setApplicationId('1204154123025387573')//
     .setType('STREAMING')//
     .setURL('https://www.youtube.com/') //
-    .setState('ايل المز')//تحط اي اسم تبيه يظهر بالحاله
+    .setState('an open-world')//تحط اي اسم تبيه يظهر بالحاله
     .setName('I fail in your love')//تحط اي اسم تبيه يظهر بالحاله
-    .setDetails(`ترا السيرفر اسطوري`)//تحط اي اسم تبيه يظهر بالحاله
+    .setDetails(`Genshin Impact)//تحط اي اسم تبيه يظهر بالحاله
     .setStartTimestamp(Date.now())
  .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1107569693373509715/1217851430300553227/ffbab8235ddc5c0290ecd6ceccc0a61a.png?ex=66058787&is=65f31287&hm=37b982b132ecde35987cac720f189c81d780fa1424c0e13acccd1c464f45badf&') //رابط الصوره الكبيره.
 
-    .setAssetsLargeText('Genshin Impact') //Text when you hover the Large image
-    .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1107569693373509715/1217851430300553227/ffbab8235ddc5c0290ecd6ceccc0a61a.png?ex=66058787&is=65f31287&hm=37b982b132ecde35987cac720f189c81d780fa1424c0e13acccd1c464f45badf&') //رابط الصوره الصغيره.
+    .setAssetsLargeText('action role-playing') //Text when you hover the Large image
+    .setAssetsSmallImage('https://cdn2.steamgriddb.com/icon/314f19f082e69886c20e31c70fe6dceb.ico') //رابط الصوره الصغيره.
     .setAssetsSmallText('Diaz') //Text when you hover the Small image
     .addButton('Talk With Me 🌸','https://discord.gg/zrQnUCuX')//تقدر تحط رابط سيرفر ديسكورد او اي رابط
     .addButton('Invite me 🌸','https://shorturl.at/sKZ57');//تقدر تحط رابط سيرفر ديسكورد او اي رابط
