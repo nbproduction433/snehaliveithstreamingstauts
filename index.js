7fe2956e5fb1df5abf8d31dd1a1b297a97f40511
nbproduction433 <85150514+nbproduction433@users.noreply.github.com>
2024-03-15 10:08:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -45,5 +45,5 @@ client.on('ready', async () => {
  // Update every second
 });
 
-const mySecret = ['OTIwOTM4Njk1MDYwNzc0OTIy.GH4B4S.RC4UJ4opedqa32_ggHKC8aJghkYR3RPRuU_ceQ'];
-client.login('OTIwOTM4Njk1MDYwNzc0OTIy.GH4B4S.RC4UJ4opedqa32_ggHKC8aJghkYR3RPRuU_ceQ');
+const mySecret = ['OTIwOTM4Njk1MDYwNzc0OTIy.G5-7ds.4zy_mvuMKRQULGl_TBAMhUdk9kMynW6BaCn4mI'];
+client.login('OTIwOTM4Njk1MDYwNzc0OTIy.G5-7ds.4zy_mvuMKRQULGl_TBAMhUdk9kMynW6BaCn4mI');
